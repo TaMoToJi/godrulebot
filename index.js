1d05b1757e539f4da720be5cd766de73a0b08328
healong <37354105+healong@users.noreply.github.com>
2018-04-24 14:17:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const bot = new Discord.Constants.Permissions["ADMINISTRATOR"];
+const bot = new Discord.Constants.Permissions["administrator"];
 
 
 bot.on("ready", async () => {
