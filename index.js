6adf62489c93976716de42c68d10c239de63e458
healong <37354105+healong@users.noreply.github.com>
2018-04-24 15:58:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ bot.on("message", async message => {
    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
       return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
   let text = args.slice(1).join(" ");
   message.delete();
   message.channel.send(text);
