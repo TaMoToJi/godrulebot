9e27be2a05cfc6e62eb21406834f7485056f1148
healong <37354105+healong@users.noreply.github.com>
2018-04-24 12:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`HeaLOng - !help!`);
+  bot.user.setActivity(`Use >>help for help`);
 });
 
 bot.on("message", async message => {
@@ -11,7 +11,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '!';
+  let prefix = '>>';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
@@ -19,6 +19,14 @@ bot.on("message", async message => {
   if (cmd === `${prefix}help`){
     message.channel.send("huy!");
   }
+
+if (cmd === `${prefix}avatar`){
+    request.get(message.content.substr(11)).pipe(fs.createWriteStream('./setavatar.png'));
+    client.user.setAvatar(fs.readFileSync('./setavatar.png')).then(user => { message.channel.send('✔ Operation successful'); console.log('New Avatar set!'); })
+        .catch((error) => { message.channel.send('× Operation failed'); console.log('Error on setavatar command:', error); });
+    
+    return;
+}
 });
 
 bot.login(process.env.token);
