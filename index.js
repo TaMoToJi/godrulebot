8009eebfd8c6c8b2c1e56d1a7da6a6402eb51c14
healong <37354105+healong@users.noreply.github.com>
2018-04-24 17:59:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -84,16 +84,6 @@ bot.on("message", async message => {
     message.reply(`𝐇𝐚𝐬 𝐁𝐞𝐞𝐧 𝐁𝐚𝐧 ${member.user.tag} 𝐅𝐫𝐨𝐦 𝐒𝐞𝐫𝐯𝐞𝐫 𝐑𝐞𝐚𝐬𝐨𝐧:${reason}`);
   }
 
- if(cmd === `${prefix}dlink`){
- message.channel.sendMessage({
-        "embed": {
-                title: 'Discord Server Links',
-                url: 'http://discord.gg/7mS9GEY',
-                "image": {
-                "url": "http://api.buienradar.nl/image/1.0/RadarMapNL?a=.gif",
-                }
-            }
-        });
 });
 
 bot.login(process.env.token);
