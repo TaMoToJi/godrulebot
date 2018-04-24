9626e9167f470607b8250395227c71530e9c2276
healong <37354105+healong@users.noreply.github.com>
2018-04-24 14:34:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ bot.on("message", async message => {
   if(cmd === `${prefix}say`){
    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.OWNER)) )
       return message.reply("Sorry, you don't have permissions to use this!");
+   let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
   let text = args.slice(1).join(" ");
   message.delete();
   message.channel.send(text);
