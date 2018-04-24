2ccb062138379251513da46766e1f72cca9c7ce8
healong <37354105+healong@users.noreply.github.com>
2018-04-24 16:58:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ bot.on("message", async message => {
   }
 
  if(cmd === `${prefix}clear`){
+    if(message.author.id !== config.ownerID) return;
     const deleteCount = parseInt(args[0], 10);
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
