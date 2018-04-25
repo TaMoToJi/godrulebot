419d4c60b8f83a76695746562c1335cb6c40c7b4
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 19:21:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -179,6 +179,7 @@ bot.on("message", async message => {
     .setDescription("[Click Here](https://tamotoji533.wixsite.com/healong) View To Website :globe_with_meridians:")
     .setURL("https://cdn.discordapp.com/attachments/364284401497931786/438579603465043975/Screenshot_2018-04-25-12-58-16.png")
     .setFooter("Powered By TaMoToJi", "https://cdn.discordapp.com/avatars/438160518293880832/0301fdd6dffcaf6751d33a809a32941d.png?size=2048")
+    .addBlankField(true)
 
     return message.channel.send(botembed);
 
