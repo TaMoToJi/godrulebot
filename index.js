f15e86f952dd0618d2ae8160719563c9bad0ed2e
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 16:48:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
-  bot.user.setActivity("in guild 21", {type: "PLAYING"});
+  bot.user.setActivity("in guild 21", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
