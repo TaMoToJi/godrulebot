e39beffb89b0202327f8c0400e4bc2ad62c5b1ba
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 16:51:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
 
-  bot.user.setActivity("in guild 21", {type: "WATCHING"});
+  bot.user.setActivity("in guild 21", {type: "Watching"});
 });
 
 bot.on("message", async message => {
@@ -12,7 +12,7 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '>>';
+  let prefix = '#';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
