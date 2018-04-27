1fb31a0980bb3a6e003aa51f7489bfa48e7fb3a7
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 22:24:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ bot.on("message", async message => {
   }
 
   if(cmd === `${prefix}prefix`) {
-   return message.channel.sand('Prefix : ${bot.user.username} is ${prefix}');
+   return message.author.reply("Prefix : ${bot.user.username} is ${prefix}");
 
   }
 
