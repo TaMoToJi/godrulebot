0fbe7caa7e53f5f1773aa004eeba8d398f699a52
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-01 18:09:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -97,27 +97,16 @@ var sayings = ["It is certain",
   
   if(comd === `${prefix}userinfo`){
     if (message.content.startsWith(PREFIX + 'Userinfo')) {
-
         const UserInfo = new Discord.MessageEmbed()
-
-            //All Fields are Optional Pick Any some
-
             .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
             .setTitle('UserInfo')
             .setURL('www.google.com') //Any Vaild Link
             .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
             .setImage(message.author.avatarURL()) //Add Any Image URl || Image
             .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
-
-            //All Feilds Are Just Examples pick Some & add as you like
-
             .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
-            .addField('AvatarURL', message.author.avatarURL({
-                format: 'png'
-            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
-            .addField('AvatarURL', message.author.avatarURL({
-                size: '2048'
-            }), true)
+            .addField('AvatarURL', message.author.avatarURL, true)
+            .addField('AvatarURL', message.author.avatarURL, true)
             .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
             .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
             .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
@@ -131,7 +120,6 @@ var sayings = ["It is certain",
             .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
             .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
             .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-
             .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
             .setTimestamp() //The timestamp of this embed
 
