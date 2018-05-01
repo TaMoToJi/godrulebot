62a61aaa552b78152eb17ebb77e2e59461bc7105
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-01 18:27:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -95,29 +95,7 @@ var sayings = ["It is certain",
 			message.channel.send({embed: ballEmb})
 }
   
-  if(cmd === `${prefix}userinfo`){
-        const UserInfo = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setTitle('UserInfo')
-            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
-            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
-            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
-            .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Presence', message.author.presence) //The presence of this user
-            .addField('Presence Status', message.author.presence.status) //The presence status of this user
-            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
-            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
-            .setTimestamp() //The timestamp of this embed
 
-            message.channel.send(UserInfo);
-   }
- 
 
   if(cmd === `${prefix}radd`) {
     let myRole = message.guild.role.find('name', "ADMIN")
@@ -145,8 +123,10 @@ var sayings = ["It is certain",
   }
 
  if(cmd === `${prefix}ping`){
- message.channel.send(new Date().getTime() - message.createdTimestamp + " ms :satellite: ");
-
+    const newemb = new Discord.RichEmbed()
+    .setColor(0xFFBF00)
+    .addField('``Ping ```', new Date().getTime() - message.createdTimestamp + " ms ")
+     message.channel.send({embed: newemb})
 }
 
 
